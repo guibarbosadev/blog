7e5caada4a7d0b1f4198103bf18578a0417adee2
Guilherme Barbosa <guilherme.barbosa@kyte.com.br>
2021-11-02 22:44:30 -0300

    Refact

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,7 @@
-import styles from "../styles/Home.module.css";
-import Posts from "../components/posts/Posts";
-import Header from "../components/header/Header";
+import { AppProps } from "next/app";
 
-const App: React.FC = () => (
-  <div className={styles.container}>
-    <div className={styles.content}>
-      <Header />
-      <Posts />
-    </div>
-  </div>
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
 );
 
 export default App;
