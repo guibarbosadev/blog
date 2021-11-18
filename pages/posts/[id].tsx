c5829cef71af08b0629f2aa7ef70445cb6aff12d
Guilherme Barbosa <guilherme.barbosa@kyte.com.br>
2021-11-17 21:25:37 -0300

    Add 'posts/[id]' page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,15 +1,34 @@
-import type { GetStaticPaths } from "next";
+import type { GetStaticPaths, GetStaticProps } from "next";
 import type { FC } from "react";
 import { getPostsIds } from "../../lib/api/posts";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 const PostDetail: FC = () => {
-  return <div>salve</div>;
+  const { t } = useTranslation("common");
+  return <div>{t("hi")}</div>;
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  let props = {};
+
+  if (locale) {
+    const translations = await serverSideTranslations(locale, ["common"]);
+    props = { ...translations };
+  }
+
+  return { props };
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
   const ptPostsIds = getPostsIds("pt-BR");
   const enPostsIds = getPostsIds("en");
-  const paths = ptPostsIds.concat(enPostsIds);
+  const postIds = ptPostsIds.concat(enPostsIds);
+  const paths = postIds.map((postName) => ({
+    params: {
+      id: postName,
+    },
+  }));
 
   return {
     paths,
